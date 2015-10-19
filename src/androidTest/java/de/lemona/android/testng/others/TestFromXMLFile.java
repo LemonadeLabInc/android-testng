7b856be036818f4214dff9926c1b62e815dfcd9d
Pier Fumagalli <pier@usrz.com>
2015-10-19 12:53:34 +0900

    Add a simple test invoked by "testng.xml".

---
+++ src/androidTest/java/de/lemona/android/testng/others/TestFromXMLFile.java
@@ -0,0 +1,11 @@
+package de.lemona.android.testng.others;
+
+import org.testng.annotations.Test;
+
+public class TestFromXMLFile {
+
+    @Test
+    public void testFromXMLFile() {
+        // Nothing to do, this should just run...
+    }
+}
